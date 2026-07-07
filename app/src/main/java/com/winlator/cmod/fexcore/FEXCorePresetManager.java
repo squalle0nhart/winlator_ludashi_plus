70b99fb81e722a85b7c44f8b75954833d715d62f
squalle0nhart.1993@gmail.com <squalle0nhart.1993@gmail.com>
2026-07-07 11:57:52 +0700

    feat: add FEXCore Performance (TSO) preset

--- app/src/main/java/com/winlator/cmod/fexcore/FEXCorePresetManager.java
+++ app/src/main/java/com/winlator/cmod/fexcore/FEXCorePresetManager.java
@@ -64,6 +64,16 @@ public class FEXCorePresetManager {
             envVars.put("FEX_DYNAMICL1CACHE", "1");
             envVars.put("FEX_DISABLEL2CACHE", "0");
         }
+        else if (id.equals(FEXCorePreset.PERFORMANCE_TSO)) {
+            envVars.put("FEX_TSOENABLED", "1");
+            envVars.put("FEX_VECTORTSOENABLED", "0");
+            envVars.put("FEX_MEMCPYSETTSOENABLED", "0");
+            envVars.put("FEX_HALFBARRIERTSOENABLED", "0");
+            envVars.put("FEX_X87REDUCEDPRECISION", "1");
+            envVars.put("FEX_MULTIBLOCK", "1");
+            envVars.put("FEX_DYNAMICL1CACHE", "1");
+            envVars.put("FEX_DISABLEL2CACHE", "0");
+        }
         else if (id.equals(FEXCorePreset.DENUVO)) {
             envVars.put("FEX_TSOENABLED", "0");
             envVars.put("FEX_VECTORTSOENABLED", "0");
@@ -92,6 +102,7 @@ public class FEXCorePresetManager {
         presets.add(new FEXCorePreset(FEXCorePreset.COMPATIBILITY, context.getString(R.string.compatibility)));
         presets.add(new FEXCorePreset(FEXCorePreset.INTERMEDIATE, context.getString(R.string.intermediate)));
         presets.add(new FEXCorePreset(FEXCorePreset.PERFORMANCE, context.getString(R.string.performance)));
+        presets.add(new FEXCorePreset(FEXCorePreset.PERFORMANCE_TSO, context.getString(R.string.performance_tso)));
         presets.add(new FEXCorePreset(FEXCorePreset.DENUVO, context.getString(R.string.denuvo)));
         for (String[] preset : customPresetsIterator(context)) presets.add(new FEXCorePreset(preset[0], preset[1]));
         return presets;
