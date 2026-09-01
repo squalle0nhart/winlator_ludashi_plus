Juan-Antonio-Doe/Winlator-Ludashi

displayx: add new option to control SurfaceControl backpressure

app/src/main/cpp/winlator/renderer/displayx.cpp
@@ -504,6 +504,10 @@ void DisplayX::presentThreadLoop() {
                    completeContext->requests.push_back(std::move(presentRequest));
                    env->CallVoidMethod(xServer->xserverDisplayActivity, cache->updateFrameRating, window->windowObj);
                 }    
+                if (!window->backPressureEnabled && pfnASurfaceTransactionSetEnableBackPressure && backPressure)  {
+                    pfnASurfaceTransactionSetEnableBackPressure(presentTransaction, window->control, true);
+                    window->backPressureEnabled = true;
+                }
             }
         }
         
@@ -638,7 +642,6 @@ void DisplayX::createWindowControl(Window *window) {
     if (pfnASurfaceControlAcquire)    
         pfnASurfaceControlAcquire(window->control);
     
-    pfnASurfaceTransactionSetEnableBackPressure(windowTransaction, window->control, false);
     pfnASurfaceTransactionSetZOrder(windowTransaction, window->control, window->z_order);
     pfnASurfaceTransactionSetVisibility(windowTransaction, window->control, ASURFACE_TRANSACTION_VISIBILITY_HIDE);
     
@@ -959,4 +962,8 @@ void DisplayX::setPerformanceMode(bool perfMode) {
 
 void DisplayX::setPresentRR(bool presentRR) {
     this->presentRR = presentRR;
+}
+
+void DisplayX::setBackPressure(bool backPressure) {
+    this->backPressure = backPressure;
 }

app/src/main/cpp/winlator/renderer/displayx.hpp
@@ -126,6 +126,7 @@ class DisplayX {
         std::atomic_bool surfaceChanged{false};
         std::atomic_bool perfMode{true};
         std::atomic_bool presentRR{true};
+        std::atomic_bool backPressure{false};
         
         bool requestUpdate = false;
         
@@ -183,4 +184,5 @@ class DisplayX {
         void toggleFullscreen();
         void setPerformanceMode(bool perfMode);
         void setPresentRR(bool presentRR);
+        void setBackPressure(bool backPressure);
 };

squalle0nhart/winlator_ludashi_plus

app/src/main/cpp/winlator/renderer/EGL/displayx.cpp
@@ -642,6 +642,10 @@ void DisplayX::presentThreadLoop() {
                 }
                 pSTSetBuffer(presentTransaction, window->control,
                              buffer, request->syncFence);
+                if (!window->backPressureEnabled && pSTSetBackPressure && backPressure) {
+                    pSTSetBackPressure(presentTransaction, window->control, true);
+                    window->backPressureEnabled = true;
+                }
                 request->fenceSubmitted = true;
                 request->syncFence = -1;
             } else {
@@ -825,8 +829,6 @@ void DisplayX::createWindowControl(Window *window) {
     window->control = pSCCreate(window->parent->control, "displayx-window");
     if (!window->control) return;
     if (pSCAcquire) pSCAcquire(window->control);
-    if (pSTSetBackPressure)
-        pSTSetBackPressure(windowTransaction, window->control, false);
     pSTSetZOrder(windowTransaction, window->control, window->zOrder);
     pSTSetVisibility(windowTransaction, window->control, SURFACE_VISIBILITY_HIDE);
 
@@ -1089,4 +1091,8 @@ void DisplayX::setPresentAtRefreshRate(bool enabled) {
     auto lock = presentLock.lock();
     presentAtRefreshRate = enabled;
     if (!enabled && !presentRequests.empty()) presentLock.notify();
+}
+
+void DisplayX::setBackPressure(bool enabled) {
+    backPressure = enabled;
 }

app/src/main/cpp/winlator/renderer/EGL/displayx.hpp
@@ -138,6 +138,7 @@ class DisplayX {
         std::atomic_bool surfaceChanged{false};
         std::atomic_bool performanceMode{true};
         std::atomic_bool presentAtRefreshRate{true};
+        std::atomic_bool backPressure{false};
 
         bool requestUpdate = false;
         int fullscreenMode = 0;
@@ -194,4 +195,5 @@ class DisplayX {
         void setFullscreenMode(int mode);
         void setPerformanceMode(bool enabled);
         void setPresentAtRefreshRate(bool enabled);
+        void setBackPressure(bool enabled);
 };